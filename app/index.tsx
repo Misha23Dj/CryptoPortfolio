232a09d812c6d65776f29c4bca4dea1ff0f6712d
Milos Djordjevic <mdjordjevic@webstaurantstore.com>
2025-08-31 14:07:15 -0500

    removed template components and utils

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const AppDisplay = () => {
+  return (
+    <View style={{ justifyContent: "center", flex: 1, alignItems: "center" }}>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default AppDisplay;
